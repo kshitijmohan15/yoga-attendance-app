a32d63a7160e072097609ccdca299e1963915bd3
kshitijmohan15 <92985242+kshitijmohan15@users.noreply.github.com>
2023-01-28 19:26:40 +0530

    deployment changes

--- src/server/trpc/router/_app.ts
+++ src/server/trpc/router/_app.ts
@@ -1,10 +1,8 @@
 import { router } from "../trpc";
 import { authRouter } from "./auth";
-import { exampleRouter } from "./example";
 import { zoomRouter } from "./zoom";
 
 export const appRouter = router({
-	example: exampleRouter,
 	auth: authRouter,
 	zoom: zoomRouter,
 });
