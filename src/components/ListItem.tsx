91c880b218a5f179ae20fe2b93513660ed537bef
kshitijmohan15 <92985242+kshitijmohan15@users.noreply.github.com>
2023-05-01 23:13:15 +0530

    added hover effects to batches

--- src/components/ListItem.tsx
+++ src/components/ListItem.tsx
@@ -1,6 +1,24 @@
 import { cva, type VariantProps } from "class-variance-authority";
-import React from "react";
+import React, { MouseEvent } from "react";
 import { twMerge } from "tailwind-merge";
+import { motion, useMotionTemplate, useMotionValue } from "framer-motion";
+
+function getHoverColor(intent: string) {
+	switch (intent) {
+		case "unpaid":
+			return "#fabfbf";
+		case "paid":
+			return "#d1fae5";
+		case "info":
+			return "#dbeafe";
+		case "paused":
+			return "#fefcbf";
+		case "default":
+			return "#e5e7eb";
+		default:
+			return "#d1fae5";
+	}
+}
 
 const ListItem = ({
 	intent,
@@ -11,22 +29,44 @@ const ListItem = ({
 	children: React.ReactNode;
 	classNames?: string;
 }) => {
+	let mouseX = useMotionValue(0);
+	let mouseY = useMotionValue(0);
+	function handleMouseMove({ clientX, clientY, currentTarget }: MouseEvent) {
+		let { left, top } = currentTarget.getBoundingClientRect();
+		mouseX.set(clientX - left);
+		mouseY.set(clientY - top);
+	}
 	return (
-		<div {...props} className={twMerge(listStyles({ intent }), classNames)}>
-			{children}
+		<div className={twMerge(listStyles({ intent }), classNames)}>
+			<motion.div
+				style={{
+					background: useMotionTemplate`radial-gradient(600px circle at ${mouseX}px ${mouseY}px, ${getHoverColor(
+						intent ?? "default"
+					)}, transparent 80%)`,
+				}}
+				className="absolute inset-0 px-4 opacity-0 transition duration-300 group-hover:opacity-80"
+				onMouseMove={handleMouseMove}
+				{...props}
+			></motion.div>
+			<div
+				onMouseMove={handleMouseMove}
+				className="z-10 flex w-full items-center px-6"
+			>
+				{children}
+			</div>
 		</div>
 	);
 };
 const listStyles = cva(
-	"flex h-28 cursor-pointer flex-row items-center rounded-md  px-4 py-6 transition duration-300 ease-in-out",
+	"flex h-28 flex-row group items-center overflow-hidden rounded-md relative flex-row transition duration-300 ease-in-out group",
 	{
 		variants: {
 			intent: {
 				default:
-					"bg-primary-light-600 text-primary-dark hover:bg-primary-light-600/30 dark:bg-primary-dark dark:text-primary-light dark:hover:bg-primary-dark-600",
-				paid: "text-green-700 bg-green-200 hover:bg-green-300 dark:text-green-200 dark:bg-green-700 dark:hover:bg-green-800",
-				unpaid: "text-red-700 bg-red-200 hover:bg-red-300 dark:text-red-200 dark:bg-red-700 dark:hover:bg-red-800",
-				paused: "text-yellow-700 bg-yellow-200 hover:bg-yellow-300 dark:text-yellow-200 dark:bg-yellow-700 dark:hover:bg-yellow-800",
+					"bg-primary-light-600 text-primary-dark dark:bg-primary-dark dark:text-primary-light dark:hover:bg-primary-dark-600",
+				paid: "text-green-700 bg-green-300 dark:text-green-200 dark:bg-green-700 dark:hover:bg-green-800",
+				unpaid: "text-red-700 bg-red-200 dark:text-red-200 dark:bg-red-700 dark:hover:bg-red-800",
+				paused: "text-yellow-700 bg-yellow-200 dark:text-yellow-200 dark:bg-yellow-700 dark:hover:bg-yellow-800",
 			},
 		},
 	}
