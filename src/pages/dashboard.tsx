37b2e8b3a8031ff4cc736bae32598c8cff69d3e5
kshitijmohan15 <92985242+kshitijmohan15@users.noreply.github.com>
2023-08-10 17:20:26 +0530

    added graph to website

--- src/pages/dashboard.tsx
+++ src/pages/dashboard.tsx
@@ -2,28 +2,33 @@ import Layout from "@/components/Layout";
 import { trpc } from "@/utils/trpc";
 import { GetServerSidePropsContext } from "next";
 import { getSession } from "next-auth/react";
-import React from "react";
-
+import { BarChart } from "@tremor/react";
+import { Skeleton } from "@/components/Skeleton";
 const Dashboard = () => {
-	const { data: hoursByStudents } =
-		trpc.analytics.getTotalHoursForStudent.useQuery();
+	const { data, isLoading } = trpc.analytics.top5Attendants.useQuery();
 	return (
 		<Layout>
-			<div className="flex flex-col gap-10">
-				<table>
-					<thead>
-						<tr>
-							<th className="text-left text-lg">Student</th>
-							<th className="text-left text-lg">Minutes</th>
-						</tr>
-						{hoursByStudents?.studentWithMinutes.map((student) => (
-							<tr key={student.id}>
-								<td>{student.name}</td>
-								<td>{student.totalMinutes}</td>
-							</tr>
-						))}
-					</thead>
-				</table>
+			<div className="flex flex-col gap-10 ">
+				<div className="w-full flex flex-col items-center">
+					<div className="flex flex-col items-start w-[70%]">
+						<h1>Most Frequent Attendants In The Last 30 Days</h1>
+						{isLoading ? (
+							<Skeleton className="h-48 bg-gray-400" />
+						) : (
+							data && (
+								<BarChart
+									className="mt-6 w-full"
+									data={data}
+									index="name"
+									categories={["count"]}
+									colors={["blue"]}
+									yAxisWidth={48}
+								/>
+							)
+						)}
+					</div>
+				</div>
+				{/* <pre>{JSON.stringify(data, null, 2)}</pre> */}
 			</div>
 		</Layout>
 	);
--- src/server/trpc/router/analytics.ts
+++ src/server/trpc/router/analytics.ts
@@ -5,43 +5,53 @@ import dayjs from "dayjs";
 import { constants } from "fs/promises";
 
 export const analyticsRouter = router({
-	getTotalHoursForStudent: protectedProcedure.query(async ({ ctx }) => {
-		const { prisma, session } = ctx;
-		const { user } = session;
+	top5Attendants: protectedProcedure.query(async ({ ctx }) => {
+		// get the student with the most number of attendances in the last week
+		const {
+			prisma,
+			session: { user },
+		} = ctx;
 
 		try {
 			const students = await prisma.student.findMany({
 				where: {
 					teacherId: user.id,
 				},
-				include: {
-					attendance: {
-						select: {
-							startDate: true,
-							endDate: true,
-						},
+				select: {
+					id: true,
+					name: true,
+				},
+			});
+			const attendances = await prisma.attendance.groupBy({
+				by: ["studentId"],
+				_count: {
+					studentId: true,
+				},
+				where: {
+					startDate: { gte: dayjs().subtract(30, "day").toDate() },
+					studentId: { in: students.map((student) => student.id) },
+				},
+				orderBy: {
+					_count: {
+						studentId: "desc",
 					},
 				},
-				take: 10,
+				take: 8,
 			});
-			const studentWithMinutes = students.map((student) => {
-				const totalMinutes = student.attendance.reduce((acc, curr) => {
-					const start = dayjs(curr.startDate);
-					const end = dayjs(curr.endDate);
-					const diff = end.diff(start, "minute");
-					return acc + diff;
-				}, 0);
-				return {
-					name: student.name,
-					totalMinutes: totalMinutes,
-					id: student.id,
-				};
-			});
-			return {
-				studentWithMinutes: studentWithMinutes.sort(
-					(a, b) => b.totalMinutes - a.totalMinutes
-				),
-			};
+
+			const finalStudents: { count: number; name: string }[] = [];
+			for (let attendance of attendances) {
+				const student = students.find(
+					(student) => student.id === attendance.studentId
+				);
+				if (student) {
+					finalStudents.push({
+						count: attendance._count.studentId,
+						name: student.name.split(" ")[0] ?? student.name,
+					});
+				}
+			}
+			return finalStudents;
 		} catch (error: any) {
 			throw new TRPCError({
 				message: error,
